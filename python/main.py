87125fdc5f5ea2564478056a6474953f1bb8f7fe
cristian gonzalez franco <97616425+cristian10gf@users.noreply.github.com>
2025-02-03 21:18:17 +0000

    Agregar implementación de generación de combinaciones de paréntesis y configuración de Docker

---
+++ python/main.py
@@ -0,0 +1,33 @@
+import time
+
+C_ncacheparentesis = {
+    0: [""],
+    1: ["()"],
+    2: ["()()", "(())"]
+}
+
+def recursiva(n):
+    if n == 0:
+        return [""]
+    elif n == 1:
+        return ["()"]
+    elif n == 2:
+        return ["()()", "(())"]
+    else:
+        if n not in C_ncacheparentesis:
+            C_ncacheparentesis[n] = []        
+            for m in range(n):
+                for p in recursiva(m):
+                    for q in recursiva(n-m):
+                        C_ncacheparentesis[n].append(p+q)
+                        C_ncacheparentesis[n].append(q+p)
+                        C_ncacheparentesis[n].append(p[0:int(len(p)/2)] + q + p[int(len(p)/2):int(len(p))]   )
+            C_ncacheparentesis[n] = list(set(C_ncacheparentesis[n]))
+        return C_ncacheparentesis[n]
+
+inicio = time.time()
+result = recursiva(12)
+fin = time.time() - inicio
+
+with open("./output.csv", "w") as file:
+    file.write("python," + str(fin) + "\n")
